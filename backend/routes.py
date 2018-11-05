003bde32eda1d8ad59003eaa32b5cb997023657a
ArkadioG <frank.gdansk@gmail.com>
2018-11-05 14:20:28 +0100

    contact route

--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,20 @@
 from backend import app
+from flask import jsonify
+
+
 
 @app.route('/')
 def home():
     return 'Hello across-stack!'
+
+
+@app.route('/contact')
+def contact():
+    author_data = {
+        "author": "Arek Gutkowski",
+        "email": "me@example.com",
+        "webpage": "example.com",
+        "twitterName": "ArkadioG"
+    }
+
+    return jsonify(author_data)
